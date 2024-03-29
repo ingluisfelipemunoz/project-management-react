089f67ad158111d759c378582c2505addfd98178
Luis Felipe Muñoz Flores <46607848+ingluisfelipemunoz@users.noreply.github.com>
2024-03-29 19:26:37 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar />
+      <NoProjectSelected />
+    </main>
+  );
+}
+
+export default App;
